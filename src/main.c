e0d2909805539b8d1fb0d7b8e2e534e8ed75a8bb
susd <susdomesticus@tutamail.com>
2024-08-05 22:31:18 +0300

    add TODO

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
 #include "common.h"
 
+// TODO: continue
+// [here](https://craftinginterpreters.com/chunks-of-bytecode.html#chunks-of-instructions)
 int main(int argc, const char *argv[]) { return 0; }
