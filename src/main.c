b7dc9e94b5b3cc6b70f5045c3497c545b6eff096
susd <susdomesticus@tutamail.com>
2024-08-06 09:11:32 +0300

    feat(Chapter 14): add chunk data struct

--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
+#include "chunk.h"
 #include "common.h"
+#include "debug.h"
 
 // TODO: continue
-// [here](https://craftinginterpreters.com/chunks-of-bytecode.html#chunks-of-instructions)
-int main(int argc, const char *argv[]) { return 0; }
+// [here](https://craftinginterpreters.com/chunks-of-bytecode.html#challenges)
+int main(int argc, const char *argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  int constant = addConstant(&chunk, 1.2);
+  writeChunk(&chunk, OP_CONSTANT, 123);
+  writeChunk(&chunk, constant, 123);
+
+  writeChunk(&chunk, OP_RETURN, 123);
+
+  disassembleChunk(&chunk, "test chunk");
+  freeChunk(&chunk);
+  return 0;
+}
